b492bc4295bbdc6c9da3e2edbac197ef92466384
liyou <liyoubdu@qq.com>
2019-07-16 14:53:43 +0800

    成功把c++测试工程运行起来。经过测试没有发现问题。

---
+++ test/c++Test/c++Test.cpp
@@ -0,0 +1,18 @@
+// c++Test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../../src/getSystemPerformance.h"
+#pragma comment(lib, "getSystemPerformance.lib")
+
+int main()
+{
+	char outStr[255];
+	int ret = hebPerformanceTest("i am main.exe", outStr, sizeof(outStr));
+	assert(ret == strlen(outStr));
+
+	printf_s(outStr);
+
+	getchar();
+	return 0;
+}
