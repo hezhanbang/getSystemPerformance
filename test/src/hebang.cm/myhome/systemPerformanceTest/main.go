3ca696e4dd55f0bd601e50ab322907ea4158da2c
liyou <liyoubdu@qq.com>
2019-07-17 15:46:47 +0800

    2

--- test/src/hebang.cm/myhome/systemPerformanceTest/main.go
+++ test/src/hebang.cm/myhome/systemPerformanceTest/main.go
@@ -2,9 +2,12 @@ package main
 
 import "C"
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
@@ -38,18 +41,56 @@ func main() {
 		return
 	}
 
-	//获取操作系统性能
-	sliceCap := int32(10240)
-	outSlice := make([]byte, sliceCap)
-	out := uintptr((unsafe.Pointer(&(outSlice[0]))))
+	for {
+		//获取操作系统性能
+		sliceCap := int32(10240)
+		outSlice := make([]byte, sliceCap)
+		out := uintptr((unsafe.Pointer(&(outSlice[0]))))
 
-	retDll, _, _ = syscall.Syscall(uintptr(statusFun), 3, 1, out, uintptr(sliceCap))
-	if retDll <= 0 {
-		log.Printf("fail to init 'getSystemPerformance.dll', ret=%d\n", retDll)
-		return
+		retDll, _, _ = syscall.Syscall(uintptr(statusFun), 3, 1, out, uintptr(sliceCap))
+		if retDll <= 0 {
+			log.Printf("fail to init 'getSystemPerformance.dll', ret=%d\n", retDll)
+			return
+		}
+
+		strLen := int(retDll)
+		jsonStr := string(outSlice[:strLen])
+		fmt.Printf("%s", jsonStr)
+
+		data := allAdapterStatus{}
+		err = jsonStr2Obj(&jsonStr, &data)
+		if nil != err {
+			log.Printf("fail to parse adapter json string, err=%s\n", err)
+			return
+		}
+
+		for _, adapter := range data.Adapters {
+			fmt.Printf("adapter[%s, %d] recvSpeed=%s, sendSpeed=%s\n", adapter.Name, adapter.MaxSpeedBit, adapter.RecvSpeed.Str, adapter.Sendpeed.Str)
+		}
+		fmt.Println("")
+
+		time.Sleep(time.Second * 2)
 	}
+}
 
-	strLen := int(retDll)
-	jsonStr := string(outSlice[:strLen])
-	fmt.Printf("%s", jsonStr)
+func jsonStr2Obj(str *string, c interface{}) (err error) {
+	err = json.Unmarshal([]byte(*str), c)
+	return
+}
+
+type allAdapterStatus struct {
+	XMLName  xml.Name      `xml:"root"`
+	Adapters []adapterInfo `xml:"adapter"`
+}
+
+type adapterInfo struct {
+	Name        string         `xml:"name"`
+	MaxSpeedBit int            `xml:"maxSpeedBit"`
+	RecvSpeed   directionSpeed `xml:"recvSpeed"`
+	Sendpeed    directionSpeed `xml:"sendSpeed"`
+}
+
+type directionSpeed struct {
+	Bit int64  `xml:"bit"`
+	Str string `xml:"str"`
 }
